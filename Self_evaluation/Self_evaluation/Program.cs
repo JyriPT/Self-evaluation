cbede9774283596318458176b5e79853c195b470
Jyri Turku <jyri.turku@student.saimia.fi>
2019-11-01 16:02:48 +0200

    First assignment done, repeats phrases 5 times.

--- Self_evaluation/Self_evaluation/Program.cs
+++ Self_evaluation/Self_evaluation/Program.cs
@@ -7,21 +7,29 @@ namespace Self_evaluation
         static void Main(string[] args)
         {
             //Kaikki tehtävät tehty yhteen, tehtävien tarkistus tehdään valikon kautta
-            string input;
+            string selection;
             Console.WriteLine("Select the assignment you wish to check (number 1-4):");
             Console.WriteLine("");
             //Laitetaan käyttäjän valinta string arvoon
-            input = Console.ReadLine();
+            selection = Console.ReadLine();
             Console.WriteLine("");
             //Käsitellään edellä annettu arvo.
             //Jos mahdollista muuttaa int arvoksi, tarkistetaan mikä numero.
             //Jos annettu arvo ei ole numero väliltä 1-4, mitään ei tapahdu.
-            if (int.TryParse(input, out int select) == true)
+            if (int.TryParse(selection, out int select) == true)
             {
                 //Tehtävä 1 valinta
                 if (select == 1)
                 {
+                    Console.WriteLine("Please input a phrase:");
+                    string input = Console.ReadLine();
 
+                    Console.WriteLine("Output:");
+
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Console.WriteLine(i + ". " + input);
+                    }
                 }
                 //Tehtävä 2 valinta
                 else if (select == 2)
